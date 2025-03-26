f2a1ad4fed1687875bdec545d753167097b72946
omkar689 <nomkar474@gmail.com>
2025-03-26 20:29:02 +0530

    3rd commit

---
+++ p1.py
@@ -0,0 +1,58 @@
+# Day 5
+
+# Comments  single line and multiline
+# djnsjkd
+''' r '''
+""" r """
+# Escape sequence
+print("hello world\nthis is me")
+print("hello world\" this is me")
+print("hello world\' this is me")
+print('hello world\" this is me')
+print("he",6 ,7 ,sep ="=",end=" 009\n")
+print("jash")
+
+
+# Day 6
+
+# everything in python is object
+
+lista= [2, "abc", 2.1, True, [6,4], ["apple", "bannana"]]
+
+tuplea= (("lion" , "tiger"), ("parrot" , "bird"))
+
+dict1= {"name": "jdoaj" ,"age": 20, "canvote": "Yes"or True}
+
+a= 1
+b= True
+c= "slkd"
+d= complex(1, 6)
+e= None
+print(type(a), type(b), type(c), type(d), type(e))
+
+
+# Day 7 & 8 exercise of calculator
+
+# Day 9 Type casting
+
+a1 = "1"
+b1 = "4"
+# c1 = "1ajs"
+
+print(int(a1) + int(b1))
+# print(int(c1) + int(b1))
+
+c1 = 1.3
+d1 = 2
+print(c1+d1)
+print(type(c1))
+print(type(d1))
+print(type(c1+d1)) 
+
+
+# Day 10
+
+a2 = int(input("Enter a no "))
+b2 = int(input("Enter a no "))
+
+print(a2 + b2)
